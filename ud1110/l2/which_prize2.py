b89b87c367f390aea6ebc913f938b363d6a12d0f
Greg Heitz <edno@edno.io>
2017-06-11 13:49:26 +0200

    l2-10 scripts

--- ud1110/l2/which_prize2.py
+++ ud1110/l2/which_prize2.py
@@ -3,19 +3,19 @@ def which_prize(points):
     Notifies a competitor of the prize they have won in a game,
     depending on the number of points they've scored
     """
-    prize = "No prize"
-    if <= 50:
+    prize = None
+    if points <= 50:
         prize = "wooden rabbit"
-    elif <= 150:
-        prize = "No prize"
-    elif <= 180:
+    elif points <= 150:
+        prize = None
+    elif points <= 180:
         prize = "wafer-thin mint"
-    elif <= 200:
+    elif points <= 200:
         prize = "penguin"
 
-    if prize == "No prize":
-        return "Oh dear, no prize this time."
-    else:
+    if prize:
         return "Congratulations! You have won a {}!".format(prize)
+    else:
+        return "Oh dear, no prize this time."
 
-print(which_prize(2000))
+print(which_prize(20))
