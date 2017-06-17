2d27aa50612a55a1f7a0fb63063c60859efc5eaf
Greg Heitz <edno@edno.io>
2017-06-17 12:41:17 +0200

    add diagonal units for diagonal sudoku

--- ud889/sudoku/utils.py
+++ ud889/sudoku/utils.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 rows = 'ABCDEFGHI'
 cols = '123456789'
 
@@ -9,7 +11,9 @@ boxes = cross(rows, cols)
 row_units = [cross(r, cols) for r in rows]
 column_units = [cross(rows, c) for c in cols]
 square_units = [cross(rs, cs) for rs in ('ABC','DEF','GHI') for cs in ('123','456','789')]
-unitlist = row_units + column_units + square_units
+ar_boxes = np.array(boxes).reshape(9,9)
+diag_units = [np.diagonal(ar_boxes).tolist()] + [np.diagonal(ar_boxes[::-1]).tolist()]
+unitlist = row_units + column_units + square_units + diag_units
 units = dict((s, [u for u in unitlist if s in u]) for s in boxes)
 peers = dict((s, set(sum(units[s],[]))-set([s])) for s in boxes)
 
